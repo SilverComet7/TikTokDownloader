c17dffdc00d62a6f2f6b6626a2aab8594ffeaad8
ChrisWang <841531691@qq.com>
2025-02-10 13:07:31 +0800

    feat

--- videoDownloadAndGroupList.js
+++ videoDownloadAndGroupList.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 const { execSync } = require('child_process');
-const { allGameList } = require('../baseAvg.js')
+const { allGameList } = require('../allGameNameList.js')
 
 
 async function downloadVideosAndGroup({
